7db651604c0c34d721fa99c15dca1dab7c5ed2e3
hellison.oliveira <hellison.oliveira@gmail.com>
2018-11-28 10:14:31 -0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router, Route
+} from 'react-router-dom'
+
+import Home from './Home'
+
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
+          <div className="container">
+            <div className="navbar-header page-scroll">
+              <a className="navbar-brand page-scroll" href="#page-top">
+                <img src="images/logo.png" height="30" />
+              </a>
+            </div>
+
+            <div className="collapse navbar-collapse navbar-ex1-collapse">
+              <ul className="nav navbar-nav">
+                <li>
+                  <a href="">Menu item</a>
+                </li>
+              </ul>
+            </div>
+
+          </div>
+        </nav>
+        <Home />
+
+
       </div>
     );
   }
---
+++ src/Home.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+
+import apis from './Api'
+
+class Home extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            genres: [],
+            isLoading: false
+        }
+    }
+
+    componentDidMount() {
+        this.setState({ isLoading: true })
+        apis.loadGenders()
+            .then((res) => {
+                this.setState({
+                    isLoading: false,
+                    genres: res.data
+                })
+            })
+    }
+
+    renderGenderLink(genre) {
+        return (
+            <span>&nbsp;<a href=''>{genre}</a>&nbsp;</span>
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                <section id="intro" className="intro-section">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-12">
+                                <h1><img src="images/logo.png" /></h1>
+                                <p>Nunca mais esqueça uma série que você assistiu ou que alguém lhe indicou.</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <section>
+                    {
+                        this.state.isLoading &&
+                        <span>Aguarde, carregando...</span>
+                    }
+                    {
+                        !this.state.isLoading &&
+                        <div>
+                            Lista de Gêneros:
+              {this.state.genres.map(this.renderGenderLink)}
+                        </div>
+                    }
+                </section>
+            </div>
+        )
+    }
+
+}
+
+export default Home
